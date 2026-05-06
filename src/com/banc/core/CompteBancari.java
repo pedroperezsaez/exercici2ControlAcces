94a2f43f48e543458e3ad6ec288099955d090361
pedroperezsaez <pedroperezsaez.1992@gmail.com>
2026-05-06 18:58:02 +0200

    haciendo

--- src/com/banc/core/CompteBancari.java
+++ src/com/banc/core/CompteBancari.java
@@ -4,14 +4,16 @@ import com.banc.model.Client;
 
 public class CompteBancari {
     // TODO 1.1: Atribut 'saldo' (double). Restringit només a aquesta classe.
+    private double saldo;
     
     // TODO 1.2: Atribut 'idCompte' (String). Accessible per aquest paquet i per subclasses d'altres paquets.
-    
+    protected  String idCompte;
     // TODO 1.3: Atribut 'titular' de tipus Client. Accessible universalment.
-
+        public Client titular;
     public CompteBancari(String id, Client titular, double saldoInicial) throws BancException {
         // TODO 1.4: Abans d'assignar els valors, crida al mètode 'validarID'. 
         // Aquest mètode ara pot llançar diferents tipus d'excepcions segons l'error detectat.
+
     }
 
     // TODO 1.9: Crea un mètode PRIVAT anomenat 'validarID' que rebi un String i retorni un boolean.
